e92f26fad5d4e142d002e9d988f855aee82811a1
Pratiksha <pratu@gmail.com>
2024-11-18 08:35:31 +0530

    All files are added

---
+++ index.js
@@ -0,0 +1,61 @@
+
+
+function makebubble()
+{
+    let bubble="";
+    let rn=0;
+    for(let i=1;i<=162;i++)
+        {    
+            rn=Math.floor(Math.random()*10);         
+            bubble+=`<div id="bubble">${rn}</div>`;
+        }
+        document.querySelector("#pbtm").innerHTML=bubble;
+}
+makebubble();
+
+
+let timer=5;
+function settimer()
+{
+    let timerInt = setInterval(function(){
+        if(timer>0)
+        {
+            timer--;
+            document.querySelector(".timer").innerHTML=timer;
+        }
+        else
+        {
+            clearInterval(timerInt);
+            document.querySelector("#pbtm").style.backgroundColor="black";
+            document.querySelector("#pbtm").innerHTML=`<h1>Game Over</h1>
+            <h2>Your score is ${score}</h2>`;
+        }
+    },1000);
+}
+settimer();
+
+let newHit=0;
+function getNewHit()
+{
+    newHit=Math.floor(Math.random()*10);
+    document.querySelector(".hit").innerHTML=newHit;
+   
+}
+getNewHit();
+let score=0;
+function increaseScore()
+{
+    score+=10;
+    document.querySelector(".score").innerHTML=score;
+}
+
+document.querySelector("#pbtm").addEventListener("click",function(dets)
+{
+    let hittedBubble=Number(dets.target.innerHTML);
+    if(newHit===hittedBubble)
+    {
+        increaseScore();
+        getNewHit();
+        makebubble();
+    }
+})
