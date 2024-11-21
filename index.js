c9c63a652c99672e32a5c52416b18f3c80360ecd
Pratiksha <pratu@gmail.com>
2024-11-21 07:46:16 +0530

    Landing page added first

--- index.js
+++ index.js
@@ -1,61 +1,14 @@
-
-
-function makebubble()
+function start()
 {
-    let bubble="";
-    let rn=0;
-    for(let i=1;i<=162;i++)
-        {    
-            rn=Math.floor(Math.random()*10);         
-            bubble+=`<div id="bubble">${rn}</div>`;
-        }
-        document.querySelector("#pbtm").innerHTML=bubble;
+    window.location.href="game.html";
 }
-makebubble();
-
-
-let timer=60;
-function settimer()
-{
-    let timerInt = setInterval(function(){
-        if(timer>0)
-        {
-            timer--;
-            document.querySelector(".timer").innerHTML=timer;
-        }
-        else
-        {
-            clearInterval(timerInt);
-            document.querySelector("#pbtm").style.backgroundColor="black";
-            document.querySelector("#pbtm").innerHTML=`<h1>Game Over</h1>
-            <h2>Your score is ${score}</h2>`;
-        }
-    },1000);
-}
-settimer();
-
-let newHit=0;
-function getNewHit()
-{
-    newHit=Math.floor(Math.random()*10);
-    document.querySelector(".hit").innerHTML=newHit;
-   
-}
-getNewHit();
-let score=0;
-function increaseScore()
-{
-    score+=10;
-    document.querySelector(".score").innerHTML=score;
-}
-
-document.querySelector("#pbtm").addEventListener("click",function(dets)
-{
-    let hittedBubble=Number(dets.target.innerHTML);
-    if(newHit===hittedBubble)
-    {
-        increaseScore();
-        getNewHit();
-        makebubble();
-    }
+gsap.from("#screenContainer h1",{
+   y:-100,
+   duration:2,
+    opacity:0,
 })
+gsap.from("#button",{
+    y:100,
+    duration:2,
+    opacity:0,
+ })
