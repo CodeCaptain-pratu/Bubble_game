3930c9b803c7d9edb13ac585487fcea12595aee9
Pratiksha <pratu@gmail.com>
2024-11-21 11:30:09 +0530

    small changes created

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ function start()
 {
     window.location.href="game.html";
 }
-gsap.from("#screenContainer h1",{
-   y:-100,
-   duration:2,
+gsap.from("#screenContainer h1 span",{
+   x:-20,
+//    duration:0.2,
     opacity:0,
+    stagger:1,
+
 })
 gsap.from("#button",{
     y:100,
-    duration:2,
+    duration:1,
     opacity:0,
  })
